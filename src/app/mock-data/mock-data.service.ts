fd82a97bb78953d176d9b96315c05fdc6b2ae4f6
Patricia Costalonga <50948965+PatriciaCostalonga@users.noreply.github.com>
2023-03-08 09:02:34 +0000

    mock data with id updated

--- src/app/mock-data/mock-data.service.ts
+++ src/app/mock-data/mock-data.service.ts
@@ -3,14 +3,33 @@ import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 
 const mockData = [
-  { title: 'Heating your home, not the planet', description: 'The future of green sustainable heating is already here', imageSrc: '.././assets/image_1.png', blob: '.././assets/blob_1.svg'},
-  { title: 'The future of heating is clean, green and affordable', description: 'Our patent pending Zero Emission Boiler (ZEB) technology is revolutionising the way we heat our homes', imageSrc: '.././assets/image_2.png', blob: '.././assets/blob_2.svg'},
-  { title: 'Use the power you generate', description: 'If you have rooftop solar PV a ZEB can automatically store excess electricity produced so you don’t unnecessarily export it to the grid', imageSrc: '.././assets/image_3.png', blob: '.././assets/blob_3.svg'}
+  { 
+    id: "section1",
+    title: 'Heating your home, not the planet', 
+    description: 'The future of green sustainable heating is already here', 
+    imageSrc: '.././assets/image_1.png', 
+    blob: '.././assets/blob_1.svg'
+  },
+  { 
+    id: "section2",
+    title: 'The future of heating is clean, green and affordable', 
+    description: 'Our patent pending Zero Emission Boiler (ZEB) technology is revolutionising the way we heat our homes', 
+    imageSrc: '.././assets/image_2.png', 
+    blob: '.././assets/blob_2.svg'
+  },
+  { 
+    id: "section3",
+    title: 'Use the power you generate', 
+    description: 'If you have rooftop solar PV a ZEB can automatically store excess electricity produced so you don’t unnecessarily export it to the grid', 
+    imageSrc: '.././assets/image_3.png', 
+    blob: '.././assets/blob_3.svg'
+  }
 ];
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class MockDataService {
   constructor() { }
 
